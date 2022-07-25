bf180cc879f839b0d37047b03c6987958260869f
Micaela <mica.v.barberis@gmail.com>
2022-07-25 11:47:44 -0300

    Contador de botón

--- src/Components/ItemListContainer/ItemListContainer.js
+++ src/Components/ItemListContainer/ItemListContainer.js
@@ -1,13 +1,41 @@
+import { useEffect } from 'react'
 import './ItemListContainer.css'
 import ItemProduct from"../ItemProduct/ItemProduct"
 
 const ItemListContainer = ({section}) =>{
+
+    const product1 = {
+        title: "Body Andressa",
+        price: 4500,
+        image: 'product5.jpg',
+        stock: 3
+    }
+    const product2 = {
+        title: "Body Natubel",
+        price: 4280,
+        image: 'producto21.jpg',
+        stock: 2
+    }
+    const product3 = {
+        title: "Body Brigitte",
+        price: 3990,
+        image: 'producto13.jpg',
+        stock: 6
+    }
+
+useEffect (() => {
+
+})
+
+const handleSubmit = () => {console.log("Envio de info")}
+
+
     return (
         <div className='list-products'>
             <h2>{section}</h2>
-            <ItemProduct title="Body Andressa" price={4500} image={'product5.jpg'}/>
-            <ItemProduct title="Body Natubel" price={4280} image={'producto21.jpg'}/>
-            <ItemProduct title="Body Brigitte" price={3990} image={'producto13.jpg'}/>
+            <ItemProduct data={product1}/>
+            <ItemProduct data={product2} action={handleSubmit}/>
+            <ItemProduct data={product3}/> 
         </div>
  )
 }
--- src/Components/ItemProduct/ItemProduct.js
+++ src/Components/ItemProduct/ItemProduct.js
@@ -1,12 +1,34 @@
+import { useState, useEffect } from 'react'
 import './ItemProduct.css'
 
-const ItemProduct =({title, price, image}) => {
+const ItemProduct = ({data, action}) => {
+    const [contador, setContador] = useState(1)
+    
+    const {title, image, price} = data
+
+    const addNumber = () => {
+        setContador(contador + 1)
+    }
+    const removeNumber = () => {
+        setContador(contador - 1)
+    }
+
+useEffect(() => {
+    console.log("Actualización")
+}, [contador])
+
+
     return (
         <div className='item-product'>
             <img className='img-product' src={`/assets/productos/${image}`} alt="Imagen producto" />
             <p>{title}</p>
             <span>$ {price}</span>
-            <button>Añadir al carrito</button>
+            <div className='countProd'>
+                <button onClick={removeNumber}>-</button>
+                <p>{contador}</p>
+                <button onClick={addNumber}>+</button>
+            </div>
+            <button onClick={action}>Añadir al carrito</button>
         </div>
     )
 }
